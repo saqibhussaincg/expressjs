7886d7f1621b5cacc75a5b86cd75dd3e00c7cd61
Saqib Hussain - Designer + Coder <saqibhussaincg@gmail.com>
2022-06-09 14:55:32 +0500

    filter

--- routes/markets.js
+++ routes/markets.js
@@ -5,18 +5,40 @@ const router = Router();
 
 const supermarket = [
     {
-        store: 'whole foods'
+        id: 1,
+        store: 'whole foods',
+        miles: 0.6
     },
     {
-        store: 'imtiaz'
+        id: 2,
+        store: 'imtiaz',
+        miles: 2.5
     },
     {
-        store: 'chase'
+        id: 3,
+        store: 'chase',
+        miles: 3
     }
 ];
 
 router.get('/', (req, res) => {
-    res.send(supermarket);
+    // console.log(req.query);
+
+    // http://localhost:5000/api/markets?miles=1
+
+    // Destructure kara miles ko or integer mai convert kara kyun k numeric value thi
+
+    // agar miles number nahi hai tou filter karo k supermarket ka data miles se kitna chota hai ya barabar hai or user ko response show karao
+    const { miles } = req.query;
+    const parsedMiles = parseInt(miles);
+    if (!isNaN(parsedMiles)) 
+    {
+
+        const filteredStores = supermarket.filter((item) => item.miles <= parsedMiles);
+        res.send(filteredStores);
+    } 
+    else  res.send(supermarket);
+   
 })
 
 module.exports = router;
